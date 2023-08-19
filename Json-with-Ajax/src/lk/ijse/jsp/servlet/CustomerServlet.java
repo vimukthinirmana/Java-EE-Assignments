ccc444d4943020171bf1468014c7c4270ceb176b
Vimukthi Nirmana Sanketh <vimukthinirmana64@gmail.com>
2023-08-19 22:27:32 +0530

    add crud

--- Json-with-Ajax/src/lk/ijse/jsp/servlet/CustomerServlet.java
+++ Json-with-Ajax/src/lk/ijse/jsp/servlet/CustomerServlet.java
@@ -44,5 +44,90 @@ public class CustomerServlet extends HttpServlet {
         }
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String cusID = req.getParameter("cusID");
+        String cusName = req.getParameter("cusName");
+        String cusAddress = req.getParameter("cusAddress");
+//        String cusSalary = req.getParameter("cusSalary");
+//        String option = req.getParameter("option");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "root", "1234");
+            PreparedStatement pstm = connection.prepareStatement("insert into Customer values(?,?,?)");
+            pstm.setObject(1, cusID);
+            pstm.setObject(2, cusName);
+            pstm.setObject(3, cusAddress);
 
+            if (pstm.executeUpdate() > 0) {
+                resp.getWriter().println("Customer Added..!");
+            }else {
+                resp.getWriter().println("Customer Not Added..!");
+            }
+
+        }catch (ClassNotFoundException e){
+            throw new RuntimeException(e);
+        }catch (SQLException e){
+            JsonObjectBuilder response = Json.createObjectBuilder();
+            response.add("state","Error");
+            response.add("message",e.getMessage());
+            response.add("data","");
+            resp.setStatus(400);
+            resp.getWriter().print(response.build());
+        }
+    }
+
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String cusID = req.getParameter("cusID");
+        String cusName = req.getParameter("cusName");
+        String cusAddress = req.getParameter("cusAddress");
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "root", "1234");
+            PreparedStatement pstm3 = connection.prepareStatement("update Customer set name=?,address=? where id=?");
+            pstm3.setObject(3, cusID);
+            pstm3.setObject(1, cusName);
+            pstm3.setObject(2, cusAddress);
+            if (pstm3.executeUpdate() > 0) {
+                resp.getWriter().println("Customer Updated..!");
+            }
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e){
+            JsonObjectBuilder response = Json.createObjectBuilder();
+            response.add("state","Error");
+            response.add("message",e.getMessage());
+            response.add("data","");
+            resp.setStatus(400);
+            resp.getWriter().print(response.build());
+        }
+    }
+
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String cusID = req.getParameter("cusID");
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "root", "1234");
+            PreparedStatement pstm2 = connection.prepareStatement("delete from Customer where id=?");
+            pstm2.setObject(1, cusID);
+            if (pstm2.executeUpdate() > 0) {
+                resp.getWriter().println("Customer Deleted..!");
+            }
+        } catch (SQLException e) {
+            JsonObjectBuilder response = Json.createObjectBuilder();
+            response.add("state","Error");
+            response.add("message",e.getMessage());
+            response.add("data","");
+            resp.setStatus(400);
+            resp.getWriter().print(response.build());
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
